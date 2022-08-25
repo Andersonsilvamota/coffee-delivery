f15489fd3bef55897a4051c3834c10750743c087
Anderson Silva Mota <andersonsilv.mota@gmail.com>
2022-08-25 14:28:36 -0300

    Navegando para a página home

--- src/pages/Checkout/components/InfoCartEmpty/index.jsx
+++ src/pages/Checkout/components/InfoCartEmpty/index.jsx
@@ -1,13 +1,24 @@
 import { ShoppingCart } from 'phosphor-react'
+import { useNavigate } from 'react-router-dom'
+
 import * as Styles from './styles'
 
 export function InfoCartEmpty() {
+  const navigate = useNavigate();
+
+  async function handleToPageInicial() {
+    await new Promise(resolve => setTimeout(resolve, 100))
+    navigate("/")
+  }
+
   return (
     <Styles.ContainerCartEmpty>
       <ShoppingCart size={50} />
       <Styles.TextoInfo>OPS... não há itens no seu carrinho. adicione algum produto na página de produtos.</Styles.TextoInfo>
       <p>Vá para página inicial ou clique no botão abaixo</p>
-      <Styles.ButtonReturnHome>VOLTAR PARA A PÁGINA INICIAL</Styles.ButtonReturnHome>
+      <Styles.ButtonReturnHome onClick={handleToPageInicial}>
+        VOLTAR PARA A PÁGINA INICIAL
+      </Styles.ButtonReturnHome>
     </Styles.ContainerCartEmpty>
   )
 }
